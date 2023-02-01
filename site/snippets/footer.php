fea6f8e51afc9f6f10f9348501ca451731c9cee2
Wenzel Massag <mail@wmassag.de>
2023-02-01 11:10:27 +0000

    Moved footer content to site.txt and updated footer positioning and size

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,3 @@
-<footer class="contain">
-	<p><a href="/legal-disclosure">Legal disclosure ("Impressum"),</a> Links marked with<span class="external">(external link)</span> point to third party websites, see <a href="/privacy">privacy policy.</a> Find me on <!-- Mastodon Verification --><a rel="me" href="https://indieweb.social/@stairjoke" target="_blank">Mastodon</a></p>
-	<p><?= $site->copyright() ?></p>
+<footer class="no-space">
+	<?= $site->footer()->kirbytext() ?>
 </footer>
